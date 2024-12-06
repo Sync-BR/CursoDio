ad5654adf3406e2e169a7dc09296e841af360161
SYNC <eduardofreitasfr@gmail.com>
2024-12-06 09:00:43 -0300

    init

--- StreamApi/src/binaryOperator/BinaryOperatorExample.java
+++ StreamApi/src/binaryOperator/BinaryOperatorExample.java
@@ -1,4 +1,14 @@
 package binaryOperator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
 public class BinaryOperatorExample {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6);
+        //Usar BinaryOperator para somar todos numeros
+        int resultado = numeros.stream().reduce(0, Integer::sum);
+        System.out.println(resultado);
+    }
 }
